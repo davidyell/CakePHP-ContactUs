4f619e33544d37903262dc4eaae497aa02389085
David Yell <dyell@ukwebmedia.com>
2013-01-03 16:32:55 +0000

    Removed database specific config.

--- Model/Contact.php
+++ Model/Contact.php
@@ -9,7 +9,6 @@ App::uses('AppModel', 'Model');
 class Contact extends AppModel {
     
     public $displayField = '';
-    public $useDbConfig = 'default';
     public $useTable = false;
     public $actsAs = array('Containable');
 
