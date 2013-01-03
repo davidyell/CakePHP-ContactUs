124ec6f02a887133cea66b264c71d35d8138cb1f
David Yell <dyell@ukwebmedia.com>
2013-01-03 16:32:37 +0000

    Added schema migration for creating the db.

---
+++ Config/Migration/1357230199_initial.php
@@ -0,0 +1,86 @@
+<?php
+class Initial extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ * @access public
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ * @access public
+ */
+	public $migration = array(
+            'up'=>array(
+                'create_table'=>array(
+                    'contacts'=>array(
+                        'id'=>array(
+                            'type'=>'integer',
+                            'null'=>false,
+                            'default'=>NULL,
+                            'key'=>'primary'
+                            ),
+                        'name'=>array(
+                            'type'=>'string',
+                            'null'=>false,
+                            'default'=>NULL,
+                            'collate'=>'utf8_general_ci',
+                            'charset'=>'utf8'
+                            ),
+                        'email'=>array(
+                            'type'=>'string',
+                            'null'=>false,
+                            'default'=>NULL,
+                            'collate'=>'utf8_general_ci',
+                            'charset'=>'utf8'
+                            ),
+                        'message'=>array(
+                            'type'=>'text',
+                            'null'=>false,
+                            'default'=>NULL,
+                            'collate'=>'utf8_general_ci',
+                            'charset'=>'utf8'
+                            ),
+                        'created'=>array('type'=>'datetime', 'null'=>true, 'default'=>NULL),
+                        'modified'=>array('type'=>'datetime', 'null'=>true, 'default'=>NULL),
+                        'indexes'=>array(
+                            'PRIMARY'=>array('column'=>'id', 'unique'=>1),
+                        ),
+                        'tableParameters'=>array('charset'=>'utf8', 'collate'=>'utf8_general_ci', 'engine'=>'InnoDB'),
+                    ),
+                ),
+            ),
+            'down'=>array(
+                'drop_table'=>array(
+                    'contacts'
+                ),
+            ),
+        );
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction, up or down direction of migration process
+ * @return boolean Should process continue
+ * @access public
+ */
+	public function after($direction) {
+		return true;
+	}
+}
